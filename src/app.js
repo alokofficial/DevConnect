297995344049faebdfdf7c7a9cad6cd7e679dc7e
alokofficial <aloknh31@gmail.com>
2025-06-07 19:14:08 +0530

    dynamic body of post method

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const connectDB = require('./config/database');
-const User = require('./model/userModel');
+const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./model/userModel");
 
 const app = express();
+app.use(express.json());
 
-app.post('/user',async(req,res)=>{
-  const newUser = {
-    firstName: "Alok",
-    lastName: "Kumar",
-    email: "aloknh31@gmail.com",
-    password: "1234",
-    age: 25,
-    gender: "male"
+app.post("/signup", async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    await newUser.save();
+    res.send("User Added Successfully!!");
+  } catch (error) {
+    console.log(error);
   }
-
-  const user = await User.create(newUser);
-  user.save();
+});
+app.get("/user", async (req, res) => {
+  const user = await User.find();
   res.send(user);
+});
 
-})
-
-app.use("/",(err,req,res,next)=>{
-  if(err){
-    res.status(500).send("Something went wrong!!")
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong!!");
   }
-})
+});
+
 connectDB()
-.then(()=>{
-  console.log("connected to database")
-  app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
   })
-})
-.catch((err)=>{
-  console.log("error",err)
-})
+  .catch((err) => {
+    console.log("error", err);
+  });
