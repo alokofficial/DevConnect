e5fd041602cce035240f358ac5fb0b762c46523e
alokofficial <aloknh31@gmail.com>
2025-06-09 20:14:23 +0530

    implemented connection request accepted, and rejected status.

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const requestRouter = express.Router();
 const connectionRequest = require('../model/connectionRequestModel')
-const User = require('../model/userModel')
+const User = require('../model/userModel');
+const ConnectionRequest = require('../model/connectionRequestModel');
 
 requestRouter.post('/request/send/:status/:toUserId', userAuth ,async(req,res)=>{
   try {
@@ -39,4 +40,29 @@ requestRouter.post('/request/send/:status/:toUserId', userAuth ,async(req,res)=>
   
 })
 
+requestRouter.post('/request/review/:status/:requestId', userAuth, async(req,res)=>{
+try {
+  const loggedInUser = req.user
+  const {status, requestId} = req.params
+  //validate status
+  const allowedStatus = ['accepted', 'rejected']
+  if(!allowedStatus.includes(status)){
+    throw new Error("Invalid Status")
+  }
+  const connectionRequest = await ConnectionRequest.findOne({
+    _id:requestId,
+    toUserId:loggedInUser._id,
+    status:'interested'
+  })
+  if(!connectionRequest){
+    throw new Error("Request not found")
+  }
+  connectionRequest.status = status
+  const data = await connectionRequest.save()
+  res.status(200).json({message:"Request Updated Successfully", data})
+
+} catch (error) {
+  res.status(400).send(error.message)
+}
+})
 module.exports = requestRouter
